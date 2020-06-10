2c1f67e5e919a374042d7b9d1fec164c8ede0ccb
Miguel Planckensteiner <49713385+mikeplanck@users.noreply.github.com>
2020-06-10 13:22:27 +0200

    Initial Commit

---
+++ Mobie/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mobie
+//
+//  Created by Miguel Planckensteiner on 10.06.20.
+//  Copyright © 2020 Miguel Planckensteiner. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
